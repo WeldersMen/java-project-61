d90e4e106870980f5c3d356f329e629b7d22434e
Nikolay Lavdansky <lavdansky1997@yandex.ru>
2023-05-23 00:31:00 +0300

    add step 5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,17 +4,22 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("0 - Exit");
+        System.out.println("""
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit
+                """);
         int gameChoice = scanner.nextInt();
         switch (gameChoice) {
             case 1 -> Greet.echoGreet();
             case 2 -> Even.evenGame();
             case 3 -> Calc.calcGame();
             case 4 -> GCD.gcdGame();
+            case 5 -> Progression.progressionGame();
             case 0 -> {
             }
             default -> System.out.println("Такой игры нет в списке.");
---
+++ app/src/main/java/hexlet/code/Progression.java
@@ -0,0 +1,40 @@
+package hexlet.code;
+import java.util.Random;
+import static hexlet.code.Engine.ARRAY_SIZE;
+import static hexlet.code.Engine.TOTAL_ROUNDS;
+import static hexlet.code.Random.randomNumber;
+public class Progression {
+    private static final  String GAME_ANSWER = "What number is missing in the progression?";
+    public static void progressionGame() {
+        String[][] questionsAndAnswers = new String[TOTAL_ROUNDS][ARRAY_SIZE]; // массив вопросов и ответов
+        Random random = new Random();
+        for (int i = 0; i < TOTAL_ROUNDS; i++) {
+         for (int j = 0; j < ARRAY_SIZE; j++) {
+             //
+             int progressionLength = random.nextInt(6) + 5;// Длинна прогрессии от 5 до 10
+             int beginProgression = randomNumber(); // Первый элемент прогрессии от 1 до 100
+             int progressionDifference = random.nextInt(10) + 1; // Разность прогрессии от 1 до 10
+             int hiddenEelement = random.nextInt(progressionLength); //индекс элемента который нужно спрятать
+             //
+             String question =  "";
+             for (int z = 0; z < progressionLength; z++) {
+                 if (z == hiddenEelement) {
+                     question = question + ".. " ;
+                 } else {
+                     int element = beginProgression + z * progressionDifference;
+                     question = question + String.valueOf(element) + " ";
+                 }
+
+             }
+             String answer = String.valueOf(beginProgression + progressionDifference * hiddenEelement);
+             questionsAndAnswers[i][0] = question;
+             questionsAndAnswers[i][1] = answer;
+         }
+     }
+
+        Engine.gameEngine(GAME_ANSWER, questionsAndAnswers);
+
+    }
+
+    }
+
